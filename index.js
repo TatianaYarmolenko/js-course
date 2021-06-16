7516532eeb695e19132d6f6ee72f695a885a69c2
ta-moli <yarnusha7@gmail.com>
2021-06-16 13:58:43 +0300

    variables

---
+++ index.js
@@ -0,0 +1,9 @@
+var student = "Tanya";
+console.log(student);
+
+var student = "tanya";
+
+
+import "./js/variables.js";
+import "./js/primitives.js";
+console.log(primitives);
---
+++ js/variables.js
@@ -0,0 +1,29 @@
+var student = "Tanya";
+console.log(student);
+
+var count;
+console.log(count);
+
+count = 1;
+console.log(count);
+count = 2;
+console.log(count);
+
+let number = 30;
+console.log(number);
+
+let age;
+console.log(age);
+
+age = 18;
+console.log(age);
+
+age = 22;
+console.log(age);
+
+const username = "Vova";
+console.log(username);
+
+// const user;
+
+// userName = "";
